cd74b35de8f7e96a8bda54e4cdec3bc81bccadd8
Matheus Neder <matheusneder@gmail.com>
2019-09-23 16:48:57 -0300

    preconditions

--- lib/assert.sh
+++ lib/assert.sh
@@ -13,7 +13,8 @@ function assert-not-empty
 	if [ -z "${!variable_name}" ]
 	then
 		echo "assert failed; '$variable_name' is empty or not defined." >&2
-		caller 0 | awk '{ print "Line number:", $1, "; function/subroutine:", $2, "; filename:", $3 }'
+		caller 0 | awk '{ print "Line number:", $1, "; function/subroutine:", $2, "; filename:", $3 }' | \
+			tee >&2
 		exit 45
 	fi
 }
