b4ae3007cd960fc2b8386e9684736dbf2c494e8c
Matheus Neder <matheusneder@gmail.com>
2019-10-01 14:20:51 -0300

    finalize step

--- lib/vsts-pr-statuses.sh
+++ lib/vsts-pr-statuses.sh
@@ -2,6 +2,9 @@
 # Depends on assert.sh; vsts.sh
 #------------------------------
 
+# Required variable: SYSTEM_WORKFOLDER
+assert-not-empty SYSTEM_WORKFOLDER
+
 # For state params, see https://docs.microsoft.com/en-us/rest/api/azure/devops/git/pull%20request%20statuses/create?view=azure-devops-rest-5.1#gitstatusstate
 
 # Param state
@@ -18,6 +21,8 @@ function friendly-state
 	fi
 	echo "$state"
 }
+
+QUALITY_STATUS_STATE_FILE="$SYSTEM_WORKFOLDER/quality_status_state"
 
 # Param pullrequest_id
 # Param state 
@@ -37,8 +42,10 @@ function pullrequest-set-quality-gate-status
 		"$report_url" \
 		"Sonarqube analysis $(friendly-state "$state")" > /dev/null	
 
-	echo "$state" > "quality_status_state"
+	echo "$state" > "$QUALITY_STATUS_STATE_FILE"
 }
+
+SIZE_STATUS_STATE_FILE="$SYSTEM_WORKFOLDER/size_status_state"
 
 # Param pullrequest_id
 # Param state 
@@ -67,8 +74,10 @@ function pullrequest-set-size-status
 		"https://arquitetura.oleconsignado.com.br/validacao-de-tamanho-de-um-pull-request/" \
 		"$description" > /dev/null	
 
-	echo "$state" > "size_status_state"
+	echo "$state" > "$SIZE_STATUS_STATE_FILE"
 }
+
+DEPLOY_PREVIEW_STATUS_STATE_FILE="$SYSTEM_WORKFOLDER/deploy_preview_status_state"
 
 # Param pullrequest_id
 # Param state
@@ -86,5 +95,37 @@ function pullrequest-set-deploy-preview-status
 		"" \
 		"Deployment preview $(friendly-state "$state")" > /dev/null	
 
-	echo "$state" > "deploy_preview_status_state"
+	echo "$state" > "$DEPLOY_PREVIEW_STATUS_STATE_FILE"
+}
+
+# Param pullrequest_id
+function reset-pr-statuses
+{
+	local pullrequest_id="$1"
+	assert-not-empty pullrequest_id
+	pullrequest-set-size-status "$pullrequest_id" "notSet"
+	pullrequest-set-quality-gate-status "$pullrequest_id" "notSet"
+	pullrequest-set-deploy-preview-status "$pullrequest_id" "notSet"
+}
+
+# Param filename
+function should-set-status-as-failed
+{
+	local filename="$1"
+	egrep -v "^succeeded|failed$" "$filename" > /dev/null 2>&1 \
+		&& return 0
+	return 1
+}
+
+# Param pullrequest_id
+function finalize-pr-statuses
+{
+	local pullrequest_id="$1"
+	assert-not-empty pullrequest_id
+	should-set-status-as-failed "$DEPLOY_PREVIEW_STATUS_STATE_FILE" && \
+		pullrequest-set-deploy-preview-status "$pullrequest_id" "error"
+	should-set-status-as-failed "$SIZE_STATUS_STATE_FILE" && \
+		pullrequest-set-size-status "$pullrequest_id" "error"
+	should-set-status-as-failed "$QUALITY_STATUS_STATE_FILE" && \
+		pullrequest-set-quality-gate-status "$pullrequest_id" "error"
 }
